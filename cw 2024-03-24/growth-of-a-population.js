0709e64f183f3feca59c0886547450ad07e51b41
soupsmagilly <136518804+soupsmagilly@users.noreply.github.com>
2024-03-23 11:21:37 -0500

    didn't save before

--- cw 2024-03-24/growth-of-a-population.js
+++ cw 2024-03-24/growth-of-a-population.js
@@ -25,3 +25,20 @@
 // Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
 
 // There are no fractions of people. At the end of each year, the population count is an integer: 252.8 people round down to 252 persons.
+
+function nbYear(p0, percent, aug, p) {
+    let years = 0;
+    let newPop = p0;
+    while(newPop < p) {
+      years++
+      newPop += Math.floor((newPop * (percent / 100)) + aug)
+    }
+    return years
+  }
+  
+  //p: p0 = current pop, percent - must convert, aug - # of people coming or leaving(can be negative), p - population goal
+  //r: integer of how many years to reach p
+  
+  //steps: 
+  //going to be a loop
+  // while poulation is < p -- p0 * percent + aug
